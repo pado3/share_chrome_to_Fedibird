96b92c75dfc2951511662fbb63176a08133c06df
pado3 <71235750+pado3@users.noreply.github.com>
2026-08-15 11:05:16 +0900

    release 4.0 - Support for "Just me" toot
    1. 自分限定投稿する際に選択肢が欄外に出ていたため、ポップアップの高さを500から610に変更しました<br />
    2. タグをいくつか追加し、注釈をコメントしました。

--- fedibird-url-share-with-hashtags.js
+++ fedibird-url-share-with-hashtags.js
@@ -17,27 +17,35 @@ javascript: (function () {
     const visibility = 'public';
 
     /* サーバタグ （デフォルト付与） */
-    const servertag = '#fedibird';
+    const servertag = '#fedibird ';
 
     /* カテゴリタグ（以下をデフォルト値とする） */
     // let categorytag = '#nowreading';
     let categorytag = '';
 
+    /* サーバーURLがx.comだった場合は、twitter.comに置換する */
+    if (url.includes('x.com')) {
+        url = url.replace('x.com', 'twitter.com');
+    }
+
     /* 以下のURLとハッシュタグのマッピングに従ってカテゴリタグを上書き */
     const taglist = {
         /* テンプレ
-        '':'#',
+        '':'#'
         '':'#',
         '':'#',
         */
-        'www.world.rugby/pacific-nations-cup/':'#PNC2025 #JapanRugby',  // PNC20xx
-        'www.rugbyworldcup.com/2025/':'#RWC2025 #JapanRugby',   // RWC2025
-        'www.rugby-japan.jp':'#JapanRugby', // 日本ラグビー協会
-        'league-one.jp':'#リーグワン',        // リーグワン
-        'kobesteelers.com':'#リーグワン',     // 神戸S
-        'rugby-rp.com':'#JapanRugby #リーグワン',   // ラグビーリパブリック
-        'news.jsports.co.jp/rugby':'#JapanRugby #リーグワン',   // J SPORTS ラグビー
-        'news.jsports.co.jp/column/rugby':'#JapanRugby #リーグワン',    // J SPORTS ラグビーコラム
+        'biwako-otsu.keizai.biz':'#滋賀県', // びわ湖大津経済新聞
+        'svns.com':'#JapanRugby',   // サクラセブンズ
+        'world.rugby/pacific-nations-cup/':'#PNC2025 #JapanRugby',  // PNC20xx
+        'rugbyworldcup.com/2027/':'#RWC2027 #JapanRugby',   // RWC2027
+        'rugby-japan.jp':'#JapanRugby #リーグワン #LeagueOne', // 日本ラグビー協会
+        'league-one.jp':'#リーグワン #LeagueOne',        // リーグワン
+        'kobesteelers.com':'#リーグワン #LeagueOne',     // 神戸S
+        'rugby-rp.com':'#JapanRugby #リーグワン #LeagueOne',   // ラグビーリパブリック
+        'news.jsports.co.jp/rugby':'#JapanRugby #リーグワン #LeagueOne',   // J SPORTS ラグビー
+        'news.jsports.co.jp/column/rugby':'#JapanRugby #リーグワン #LeagueOne',    // J SPORTS ラグビーコラム
+        'justrugby.jp':'#JapanRugby #リーグワン #LeagueOne', // ジャストラグビー
         'tobooks':'#本好きの下剋上',    // TOBOOKS、本好きの下剋上以外の作品に注意
         'booklove-anime.jp':'#本好きの下剋上', // 本好きの下剋上アニメ公式
         'ncode.syosetu.com/n4750dy':'#本好きの下剋上',  // なろう ハンネローレ
@@ -45,13 +53,13 @@ javascript: (function () {
         'mypage.syosetu.com/mypageblog/list/userid/372556':'#本好きの下剋上',   // 香月美夜先生活動報告（割烹）
         'mypage.syosetu.com/mypageblog/view/userid/372556':'#本好きの下剋上',   // 香月美夜先生活動報告（割烹）
         'mypage.syosetu.com/372556':' #本好きの下剋上',     // 香月美夜先生トップ
-        'www.tbsradio.jp':'#ss954', // 荻上チキ・ Session
-        'www.jaxa.jp':'#jaxa',  // JAXA
-        'www.jma.go.jp':'#気象庁', // 気象庁
-        'www.nicovideo.jp':'#nowwatching',  // ニコニコ動画
-        'seiga.nicovideo.jp':'#nowwatching',    // ニコニコ静画
+        'tbsradio.jp':'#ss954', // 荻上チキ・ Session
+        'jaxa.jp':'#JAXA',  // JAXA
+        'jma.go.jp':'#気象庁', // 気象庁
+        'seiga.nicovideo.jp':'#ニコニコ漫画',   // ニコニコ静画=ニコニコ漫画
+        'nicovideo.jp':'#nowwatching',  // ニコニコ動画
         'live.nicovideo.jp':'#nowwatching',   // ニコニコ生放送
-        'www.youtube.com':'#nowwatching',   // YouTube
+        'youtube.com':'#nowwatching',   // YouTube
         'music.youtube.com':'#nowplaying'   // YouTube Music
     };
     for (key in taglist){
@@ -69,20 +77,29 @@ javascript: (function () {
         tags = tags + ' ' + title.match(hashtagsearch).join('','').replace(',','').trim();
 
     /* タイトルのハッシュを置換してハッシュタグリンク化を回避 */
-    title = title.replace(hashsearch,'＃');
+    title = title.replace(hashsearch,'(シャープ)');
 
     /* URLにクエリがある場合の処理 */
-    /* 最初のクエリを取り除いてはいけないURL（いまのところYouTubeのみ） */
+    /* テキストフラグメントも維持しようと考えたが、JavascriptでURLから取得できないのでボツ */
+    /* クエリを取り除かないURL（YouTube, Radiko, リーグワン, rugbypass.com, Bリーグ, NDL, BBCニュース, Google play） */
     const keepquerylist = [
-        'www.youtube.com',
-        'youtu.be'
+        'youtube.com',
+        'youtu.be',
+        'radiko.jp',
+        'league-one.jp',
+        'rugbypass.com',
+        'www.bleague.jp',
+        'ndl.go.jp',
+        'bbc-tv.co.jp',
+        'play.google.com'
     ];
     /* クエリにヒットする正規表現 */
     let urlsearch = /\?.*$/;
     for (const key of keepquerylist) {
         if (url.includes(key)) {
-            /* 最初のクエリをスルーし、2つ目以後のクエリにヒットする正規表現 */
-            urlsearch = /\&.*$/;
+            /* 最初のクエリをスルーし、2つ目以後のクエリにヒットする正規表現（メモ用に残す） */
+            /* urlsearch = /\&.*$/; */
+            urlsearch = ""; // クエリを取り除かない（YouTubeの開始時間などが消えるとマズイ）
         }
     }
     /* URLのクエリを取り除く */
@@ -94,7 +111,7 @@ javascript: (function () {
     /* _スラッシュ改行・タイトル・ハイフォン・URL・改行x2・タグ */
     const text = ' /\n' + title + ' - ' + url + '\n\n' + tags;
 
-    /* 投稿フォーム画面を開く（ポップアップ） */
+    /* 投稿フォーム画面を開く（ポップアップ） 自分限定投稿のために、高さを500→610に変更 */
     window.open(`https://${servername}/share?visibility=${visibility}&text=${encodeURIComponent(text)}`,
-                '_blank','width=400,height=500,left=100,top=100');
+                '_blank','width=400,height=610,left=100,top=100');
 })();
